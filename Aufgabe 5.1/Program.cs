972781dbbf8f4f4fc39801459aefe008e8ed543b
HaticeTueysuez <34030703+HaticeTueysuez@users.noreply.github.com>
2018-11-16 22:44:36 +0100

    Aufgabe 5

---
+++ Aufgabe 5.1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Aufgabe_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte einen Satz eingeben");
+            Console.Write("> ");
+            var _userInput = Console.ReadLine();
+
+            string letters = reverseLetters(_userInput);
+            string words = reverseWords(_userInput);
+            string sentence = reverseSentence(_userInput);
+
+            Console.WriteLine(letters);
+            Console.WriteLine(words);
+            Console.WriteLine(sentence);
+        }
+
+        public static string reverseLetters(string _userInput)
+        {
+            List<char> listOfLetters = new List<char>();
+
+            for (int i = _userInput.Length - 1; i > -1; i--)
+            {
+                listOfLetters.Add(_userInput[i]);
+            }
+
+            return string.Join("", listOfLetters);
+        }
+
+        public static string reverseWords(string _userInput)
+        {
+            List<string> listOfWords = new List<string>();
+            String[] splittedWords = _userInput.Split(' ');
+
+            for (int i = 0; i < splittedWords.Length; i++)
+            {
+                listOfWords.Add(splittedWords[i]);
+
+            }
+
+            listOfWords.Reverse();
+
+            return string.Join(" ", listOfWords.ToArray());
+        }
+
+        public static string reverseSentence(string _userInput)
+        {
+            string reversedWord = reverseWords(_userInput);
+            string reversedSentence = reverseLetters(reversedWord);
+
+            return reversedSentence;
+        }
+
+    }
+}
---
+++ Aufgabe 5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Aufgabe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte einen Satz eingeben");
+            string text = Console.ReadLine();
+            string letters = reverseLetters(text);
+            string words = reverseWords(text);
+            string sentence = reverseSentence(text);
+            Console.WriteLine(letters);
+            Console.WriteLine(words);
+            Console.WriteLine(sentence);
+
+        }
+
+        public static string reverseLetters(string text)
+        {
+            char[] letters = text.ToCharArray();
+            Array.Reverse(letters);
+            text = String.Join("", letters);
+            return text;
+        }
+
+        public static string reverseWords(string text)
+        {
+            string[] words = text.Split(' ');
+            Array.Reverse(words);
+            text = String.Join(" ", words);
+            return text;
+        }
+        public static string reverseSentence(string text)
+        {
+            string[] wordText = text.Split(' ');
+            string turnWord = "";
+            string[] finalText = new string[wordText.Length];
+
+            for (int i = 0; i <= wordText.Length - 1; i++)
+            {
+                turnWord += wordText[i];
+
+                string wordIsTurned = "";
+                for (int k = turnWord.Length - 1; k >= 0; k--)
+                {
+                    wordIsTurned += turnWord[k];
+                }
+                turnWord = "";
+                finalText[i] = wordIsTurned;
+
+            }
+            text = String.Join(" ", finalText);
+            return text;
+
+        }
+
+    }
+}
